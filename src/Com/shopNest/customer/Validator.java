29a95135ae18a2362dd87a84f77e50d788e43f77
nayan <nayan@DESKTOP-N8KL3C6>
2023-09-26 16:01:13 +0530

    First commit

---
+++ src/Com/shopNest/customer/Validator.java
@@ -0,0 +1,18 @@
+package Com.shopNest.customer;
+
+import ComshopNest.dbHandler.DataFetcher;
+
+public class Validator {
+
+	public static boolean isValid(String uname, String pass) {
+			String dbPass=DataFetcher.fetchPassword(uname);
+			if(pass.equals(dbPass)) {
+				return true;
+			}
+			else {
+				return false;
+			}
+	}
+}
+
+
